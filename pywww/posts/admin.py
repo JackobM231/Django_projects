2700f4b4e562b44d1eea53efd0b44bb24b8c02bb
JackobM231 <spammms231@gmail.com>
2021-11-18 13:59:44 +0100

    Posts application
    Adding:
    -superuser
    -new user
    -posts application

---
+++ pywww/posts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+admin.site.register(Post)
+# rejestracja w panelu administracyjnym modeli (sprawienie że są widoczne)
